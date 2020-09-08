9404c0ebc8afe50f56894e87f0da144492cf0b1f
Srdan Cvijetic <scwebdevsc@gmail.com>
2020-09-09 01:06:52 +0200

    Update for SCwebdevSC Portfolio Website

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function hideMenu() {
+  let menuOpen = document.querySelector(".toggler").checked;
+
+  if ((menuOpen = true)) {
+    document.querySelector(".toggler").checked = false;
+  }
+}
+window.addEventListener("scroll", hideMenu);
